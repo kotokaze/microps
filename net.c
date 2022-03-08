72401be04d88d2ccd46ff42c124a2c3ab9a7210a
kotokaze <62094392+kotokaze@users.noreply.github.com>
2022-03-08 03:11:52 +0000

    step16

--- net.c
+++ net.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <string.h>
+#include <sys/time.h>
 
 #include "platform.h"
 
@@ -21,12 +22,20 @@ struct net_protocol_queue_entry {
   uint8_t data[];
 };
 
+struct net_timer {
+  struct net_timer *next;
+  struct timeval interval;
+  struct timeval last;
+  void (*handler)(void);
+};
+
 /*
  * NOTE: if you want to add/delete the entries,
  *       you need to protect the table with a mutex
  */
 static struct net_device *devices;
 static struct net_protocol *protocols;
+static struct net_timer *timers;
 
 struct net_device *
 net_device_alloc(void)
@@ -181,6 +190,48 @@ net_protocol_register(uint16_t type, void (*handler)(const uint8_t *data, size_t
   proto->next = protocols;
   protocols = proto;
   infof("registered, type=0x%04x", type);
+  return 0;
+}
+
+// NOTE: must not call after net_run()
+int
+net_timer_register(struct timeval interval, void (*handler)(void))
+{
+  struct net_timer *timer;
+
+  timer = memory_alloc(sizeof(*timer));
+  if (!timer)
+  {
+    errorf("memory_alloc() failure");
+    return -1;
+  }
+
+  timer->interval = interval;
+  gettimeofday(&timer->last, NULL);
+  timer->handler = handler;
+  timer->next = timers;
+  timers = timer;
+  infof("registered: interval={%d, %d}", interval.tv_sec, interval.tv_usec);
+  return 0;
+}
+
+int
+net_timer_handler(void)
+{
+  struct net_timer *timer;
+  struct timeval now, diff;
+
+  for (timer = timers; timer; timer = timer->next)
+  {
+    gettimeofday(&now, NULL);
+    timersub(&now, &timer->last, &diff);
+    if (timercmp(&timer->interval, &diff, <) != 0)
+    {
+      timer->handler();
+      timer->last = now;
+    }
+  }
+
   return 0;
 }
 
